5d1b830f90ffb7a0cbe5f43c4a657dcc17745505
ambrose214 <ambrose.kuo@hotmail.com>
2019-07-18 03:38:46 -0400

    static site

---
+++ precache-manifest.f5e22b08763904da296121fcfed253c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6201a6f993c80effb9c60f0157abf52",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e73742a62051615c0c8",
+    "url": "./static/css/main.822caae0.chunk.css"
+  },
+  {
+    "revision": "656bfbf0cd5eaba26e07",
+    "url": "./static/js/2.bff5ae15.chunk.js"
+  },
+  {
+    "revision": "2e73742a62051615c0c8",
+    "url": "./static/js/main.0fb8875a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
